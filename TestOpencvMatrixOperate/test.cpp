e73ee8fcf2665488e3f79a1b68da9cee71ec7be1
sclin <read0651@hotmail.com>
2023-06-08 21:43:03 +0800

    Fixed matrix multiple problem. OpenCV only allow float matrix.

--- TestOpencvMatrixOperate/test.cpp
+++ TestOpencvMatrixOperate/test.cpp
@@ -1,6 +1,35 @@
 #include "pch.h"
 
-TEST(TestOpencvMatrixOperate, TestMatrixMultiple) 
+TEST(TestOpencvMatrixOperate, TestFloatMatrixMultiple) 
+{
+	// ARRANGE
+	float* arr1 = new float[6] {1, 2, 3, 4, 5, 6};
+	float* arr2 = new float[6] {1, 2, 3, 4, 5, 6};
+	float* arr3 = new float[4] {22, 28, 49, 64};
+	// opencv matrix multiple only allow float type
+	// please refer to https://stackoverflow.com/questions/58438557/opencv-simple-2d-matrix-multiplication-fails
+	cv::Mat expected = cv::Mat(2, 2, CV_32FC1, arr3);
+
+	// ACT
+	cv::Mat a = cv::Mat(2, 3, CV_32FC1, arr1);
+	std::cout << a << std::endl;
+	cv::Mat b = cv::Mat(3, 2, CV_32FC1, arr2);
+	std::cout << b << std::endl;
+	cv::Mat c = a * b;
+	std::cout << c << std::endl;
+
+	// ASSERT
+	size_t len = c.rows * c.cols;
+	float* c_ptr = (float*)((void*)c.data);
+	float* expected_ptr = (float*)((void*)expected.data);
+	for (int i = 0; i < len; i++)
+		EXPECT_EQ(*(c_ptr + i), *(expected_ptr + i));
+	delete[] arr1;
+	delete[] arr2;
+	delete[] arr3;
+}
+
+TEST(TestOpencvMatrixOperate, TestIntMatrixMultipleFail) 
 {
 	// ARRANGE
 	int* arr1 = new int[6] {1, 2, 3, 4, 5, 6};
@@ -13,28 +42,10 @@ TEST(TestOpencvMatrixOperate, TestMatrixMultiple)
 	std::cout << a << std::endl;
 	cv::Mat b = cv::Mat(3, 2, CV_32SC1, arr2);
 	std::cout << b << std::endl;
-	cv::Mat c = a * b;
+	// opencv matrix multiple only allow float type
+	// please refer to https://stackoverflow.com/questions/58438557/opencv-simple-2d-matrix-multiplication-fails
+	EXPECT_ANY_THROW(cv::Mat c = a * b;);
 
-	//// ARRANGE
-	//int arr1[2][3] = { {1, 2, 3}, {4, 5, 6 } };
-	//int arr2[3][2] = { {1, 2}, {3, 4}, {5, 6} };
-	//int arr3[2][2] = { {22, 28}, {49, 64 } };
-	//cv::Mat expected = cv::Mat(2, 2, CV_32SC1, arr3);
-
-	//// ACT
-	//cv::Mat a = cv::Mat(2, 3, CV_32SC1, arr1);
-	//std::cout << a << std::endl;
-	//cv::Mat b = cv::Mat(3, 2, CV_32SC1, arr2);
-	//std::cout << b << std::endl;
-	//cv::Mat c = a* b;
-
-	// ASSERT
-	//EXPECT_EQ(c, expected);
-	size_t len = c.rows * c.cols;
-	int* c_ptr = (int*)((void*)c.data);
-	int* expected_ptr = (int*)((void*)expected.data);
-	for (int i = 0; i < len; i++)
-		EXPECT_EQ(*(c_ptr + i), *(expected_ptr + i));
 	delete[] arr1;
 	delete[] arr2;
 	delete[] arr3;
@@ -43,13 +54,42 @@ TEST(TestOpencvMatrixOperate, TestMatrixMultiple)
 TEST(TestOpencvMatrixOperate, TestMatrixMultipleWithErrorRowAndCol) 
 {
 	// ARRANGE
-	int* arr1 = new int[6] {1, 2, 3, 4, 5, 6};
-	int* arr2 = new int[6] {1, 2, 3, 4, 5, 6};
+	float* arr1 = new float[6] {1, 2, 3, 4, 5, 6};
+	float* arr2 = new float[6] {1, 2, 3};
 
 	// ACT
-	cv::Mat a = cv::Mat(2, 3, CV_32SC1, arr1);
-	cv::Mat b = cv::Mat(3, 2, CV_32SC1, arr2);
+	cv::Mat a = cv::Mat(2, 3, CV_32FC1, arr1);
+	cv::Mat b = cv::Mat(3, 1, CV_32FC1, arr2);
 
 	// ASSERT
-	EXPECT_ANY_THROW(cv::Mat c = a.mul(b););
+	EXPECT_ANY_THROW(cv::Mat c = b * a;);
+}
+
+TEST(TestOpencvMatrixOperate, TestFloatMatrixMultipleWithDecimalPoint) 
+{
+	// ARRANGE
+	float* arr1 = new float[6] {1.2, 2, 3, 4, 5, 6};
+	float* arr2 = new float[6] {1.2, 2, 3, 4, 5, 6};
+	float* arr3 = new float[4] {22.440001, 28.4, 49.799999, 64};
+	// opencv matrix multiple only allow float type
+	// please refer to https://stackoverflow.com/questions/58438557/opencv-simple-2d-matrix-multiplication-fails
+	cv::Mat expected = cv::Mat(2, 2, CV_32FC1, arr3);
+
+	// ACT
+	cv::Mat a = cv::Mat(2, 3, CV_32FC1, arr1);
+	std::cout << a << std::endl;
+	cv::Mat b = cv::Mat(3, 2, CV_32FC1, arr2);
+	std::cout << b << std::endl;
+	cv::Mat c = a * b;
+	std::cout << c << std::endl;
+
+	// ASSERT
+	size_t len = c.rows * c.cols;
+	float* c_ptr = (float*)((void*)c.data);
+	float* expected_ptr = (float*)((void*)expected.data);
+	for (int i = 0; i < len; i++)
+		EXPECT_FLOAT_EQ(*(c_ptr + i), *(expected_ptr + i));
+	delete[] arr1;
+	delete[] arr2;
+	delete[] arr3;
 }
