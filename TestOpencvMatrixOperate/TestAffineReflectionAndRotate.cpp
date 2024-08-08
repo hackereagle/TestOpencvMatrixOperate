b355f6db0bc6d44f2f6db5d3b9104b2ac8053f27
sclin <read0651@hotmail.com>
2024-08-08 19:08:25 +0800

    Added testing scenarios: 1. TestSolveFromPointsSimulatePatternResult is simulate that compute    transformation matrix with points which are computed from pattern    match. The result from pattern match will shift same pixels. 2. TestSolveFromPointsWithScale is simulate pixel rate. 3. TestSolveFromPointsWithScaleUseDelta is simulate that scenario have 1    and 2.

--- TestOpencvMatrixOperate/TestAffineReflectionAndRotate.cpp
+++ TestOpencvMatrixOperate/TestAffineReflectionAndRotate.cpp
@@ -128,3 +128,149 @@ TEST(TestAffineReflectionAndRotate, TestSolveFromPoints_WithTranslate)
 	EXPECT_NEAR(*((double*)img2Stg.data + 8),  1.0, 0.001);
 	DoVectorAssert(img2Stg);
 }
+
+TEST(TestAffineReflectionAndRotate, TestSolveFromPoints_SimulatePatternResultDiff)
+{
+	// ARRANGE
+	double stagePoints[4][3] =
+	{
+		{700, 600, 1},
+		{4600, 800, 1},
+		{800, 4500, 1},
+		{4700, 4800, 1},
+	};
+	double imagePoints[4][3] =
+	{
+		{100, 4400, 1},
+		{4000, 4200, 1},
+		{200, 500, 1},
+		{4100, 200, 1},
+	};
+	cv::Mat stagePts, imagePts;
+	for (int i = 0; i < 4; i++) {
+		stagePts.push_back(cv::Mat(1, 3, CV_64FC1, stagePoints[i]));
+		imagePoints[i][0] = imagePoints[i][0] + 10;
+		imagePoints[i][1] = imagePoints[i][1] + 20;
+		imagePts.push_back(cv::Mat(1, 3, CV_64FC1, imagePoints[i]));	
+	}
+	stagePts = stagePts.t();
+	imagePts = imagePts.t();
+
+	// ACT
+	cv::Mat meta;
+	cv::invert(imagePts, meta, cv::DecompTypes::DECOMP_SVD);
+	cv::Mat img2Stg = stagePts * meta;
+	std::cout << img2Stg << std::endl;
+
+	// ASSERT
+	EXPECT_NEAR(*((double*)img2Stg.data + 0),  1.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 1),  0.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 2),  600.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 3),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 4), -1.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 5),  5000.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 6),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 7),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 8),  1.0, 0.001);
+	DoVectorAssert(img2Stg);
+}
+
+TEST(TestAffineReflectionAndRotate, TestSolveFromPointsWithScale)
+{
+	// ARRANGE
+	double stagePoints[4][3] =
+	{
+		{100, 100, 1},
+		{4000, 300, 1},
+		{200, 4000, 1},
+		{4100, 4300, 1},
+	};
+	double imagePoints[4][3] =
+	{
+		{100, 4400, 1},
+		{4000, 4200, 1},
+		{200, 500, 1},
+		{4100, 200, 1},
+	};
+	cv::Mat stagePts, imagePts;
+	for (int i = 0; i < 4; i++) {
+		stagePoints[i][0] = stagePoints[i][0] * 2;
+		stagePoints[i][1] = stagePoints[i][1] * 2;
+		stagePts.push_back(cv::Mat(1, 3, CV_64FC1, stagePoints[i]));
+		imagePts.push_back(cv::Mat(1, 3, CV_64FC1, imagePoints[i]));	
+	}
+	stagePts = stagePts.t();
+	imagePts = imagePts.t();
+
+	// ACT
+	cv::Mat meta;
+	cv::invert(imagePts, meta, cv::DecompTypes::DECOMP_SVD);
+	cv::Mat img2Stg = stagePts * meta;
+	std::cout << img2Stg << std::endl;
+
+	// ASSERT
+	EXPECT_NEAR(*((double*)img2Stg.data + 0),  2.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 1),  0.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 2),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 3),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 4), -2.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 5),  4500.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 6),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 7),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 8),  1.0, 0.001);
+	//DoVectorAssert(img2Stg);
+}
+
+TEST(TestAffineReflectionAndRotate, TestSolveFromPointsWithScaleUseDelta)
+{
+	// ARRANGE
+	double stagePoints[4][3] =
+	{
+		{100, 100, 1},
+		{4000, 300, 1},
+		{200, 4000, 1},
+		{4100, 4300, 1},
+	};
+	double imagePoints[4][3] =
+	{
+		{100, 4400, 1},
+		{4000, 4200, 1},
+		{200, 500, 1},
+		{4100, 200, 1},
+	};
+	cv::Mat stagePts, imagePts;
+	for (int i = 0; i < 4; i++) {
+		stagePoints[i][0] = stagePoints[i][0] * 2;
+		stagePoints[i][1] = stagePoints[i][1] * 2;
+		//stagePts.push_back(cv::Mat(1, 3, CV_64FC1, stagePoints[i]));
+		//imagePts.push_back(cv::Mat(1, 3, CV_64FC1, imagePoints[i]));	
+	}
+	for (int i = 1; i < 4; i++) {
+		stagePoints[i][0] = stagePoints[i][0] - stagePoints[0][0];
+		stagePoints[i][1] = stagePoints[i][1] - stagePoints[0][1];
+		stagePts.push_back(cv::Mat(1, 3, CV_64FC1, stagePoints[i]));
+		imagePoints[i][0] = imagePoints[i][0] - imagePoints[0][0];
+		imagePoints[i][1] = imagePoints[i][1] - imagePoints[0][1];
+		imagePts.push_back(cv::Mat(1, 3, CV_64FC1, imagePoints[i]));	
+	}
+	stagePts = stagePts.t();
+	imagePts = imagePts.t();
+
+	// ACT
+	cv::Mat meta;
+	cv::invert(imagePts, meta, cv::DecompTypes::DECOMP_SVD);
+	cv::Mat img2Stg = stagePts * meta;
+	std::cout << img2Stg << std::endl;
+
+	// ASSERT
+	EXPECT_NEAR(*((double*)img2Stg.data + 0),  2.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 1),  0.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 2),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 3),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 4), -2.0, 0.001);
+	//EXPECT_NEAR(*((double*)img2Stg.data + 5),  4500.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 6),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 7),  0.0, 0.001);
+	EXPECT_NEAR(*((double*)img2Stg.data + 8),  1.0, 0.001);
+	//DoVectorAssert(img2Stg);
+}
